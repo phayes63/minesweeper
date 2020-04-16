c7e47c91882e0cc740e4fce96710296b47d7a1b6
patrick hayes <screensnpicks@gmail.com>
2020-04-15 22:12:15 -0400

    initial tile class set up and tested, need to build out more as board is built

--- tile.rb
+++ tile.rb
@@ -1,4 +1,28 @@
+require "colorize"
+
 class Tile
 
+    def initialize(is_bomb, start_symbol)
+        @start_symbol = start_symbol
+        @face = start_symbol
+        @is_bomb = is_bomb
+        @flagged = [false, true]
+        @revealed = false
+    end
+
+    def reveal
+        @revealed = true
+        @is_bomb == false ? @face = "_" : @face = "!"
+    end
+
+    def flag
+        @flagged.rotate!
+        @flagged[0] == true ? @face = "F" : @start_symbol
+    end 
+
+    def proximity
+        @face = (1..8).to_a
+        # Not sure about this yet...
+    end
 
 end
