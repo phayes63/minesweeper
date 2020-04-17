0bb6e739503c75145f01ae9327d2248016e4b262
patrick hayes <screensnpicks@gmail.com>
2020-04-16 22:23:34 -0400

    created board, added tiles, planted bombs, started user-facing grid printout, this time with board file added to commit

--- Board.rb
+++ Board.rb
@@ -1,4 +1,47 @@
+require_relative "tile"
+
 class Board
 
+    attr_reader :grid, :make_pairs
+
+    def initialize
+        @grid = Array.new(9) {Array.new(9)}
+        (0..8).each do |tile|
+            @grid.each { |row| row[tile] = Tile.new(false, "*")} 
+       end
+       self.make_pairs
+       self.plant_bombs
+    end
+
+    def make_pairs
+        bomb_pairs = []
+        until bomb_pairs.length == 10
+            sample = (0..8).to_a.sample(2)
+            bomb_pairs << sample if !bomb_pairs.include?(sample)
+        end
+        bomb_pairs
+    end
+
+    def plant_bombs
+        self.make_pairs.each do |bomb|
+            x, y = bomb
+            @grid[x][y].make_bomb
+        end
+    end
+
+    def render
+        puts ""
+        puts "Minesweeper".center(20)
+        puts ""
+        puts "-" * 20
+        puts @grid.map { |row| row.map { |tile| tile.face.rjust(2) }.join }
+        puts "-" * 20
+    end
+
+    def reveal
+        @grid.each do |row|
+            row.each { |tile| tile.reveal }
+        end
+    end
 
 end
